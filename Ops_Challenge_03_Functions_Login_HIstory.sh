02c3979a091f0f84294d53e017f9fc106e67d70e
shannon <shannon@ubuntudev>
2022-07-28 02:34:03 +0000

    this is a function script that prints the login history of the server

---
+++ Ops_Challenge_03_Functions_Login_HIstory.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Script: Ops_Challenge_03_Functions_Login_History
+# Author: Shay Crane
+# Date of latest revisions 07/27/2022
+# Purpose: Print login history of lab PC using functions and/or variables
+function print_login_history {
+    last user
+    last}
+    
+# invocation:
+print_login_history
