5cf7bbed9bbe8d95ebf715b76c4b15837c8c941e
shannon <shannon@ubuntudev>
2022-07-28 21:26:03 +0000

    this commit uploads helloworld to the correct repo

---
+++ helloworld.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Script:                       	Ops 201 Class 02 Ops Challenge 02
+# Author:                       	Shay Crane
+# Date of latest revision:     7/26/2022
+
+# Purpose:     			Print a string to the terminal
+# Main
+var="Hello World"
+
+#print var
+echo "$var" 
+
+# End
