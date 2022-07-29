b26fd05734a36e26fe472a79803089275369d741
shannon <shannon@ubuntudev>
2022-07-29 05:40:29 +0000

    draft of ops challenge 04

---
+++ Ops_Challenge_04.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Script: Ops Challenge 04
+# Author: Jon Salhus
+# Date of latest revision: 07/28/2022
+# Purpose: a script that creates 4 directories using an array
+
+# NOTE: All is commented out, as I began running out of time
+#       and wanted to keep a placeholder without running a wacky script. 
+
+# define an array with 4 directories inside
+
+# create 4 directories
+
+
+# mkdir ~/201_Ops_Challenges/d0
+# mkdir ~/201_Ops_Challenges/d1
+# mkdir ~/201_Ops_Challenges/d2
+# mkdir ~/201_Ops_Challenges/d3
+
+# define an array 
+
+#first_array=(d0 d1 d2 d3)
+
+
+
+# reference an array, put files into matching directories
+
+# $[first=array3]
+
+
+
+
+
+
