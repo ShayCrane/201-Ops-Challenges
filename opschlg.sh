0916bdb89862235878faa877db7445b412a8e84d
shannon <shannon@ubuntudev>
2022-07-30 06:16:05 +0000

    draft of ops challenge 05

---
+++ opschlg.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# write a script that displays running processes, asks the user for a PID,
+# then kills the process with that PID. 
+#
+# 	for this part of the lab, initialize a process that wont harm the OSs
+#	 functionality upon termination.  dont kill esst'l processes required 
+#	for OS to work: kernel, drivers...
+
+# Use a loop in the script. 
+# 	This loop will facilitate the menu system of your script, so that it can
+#	prompt the user to choose an option. 
+# 
+# Submit github link to your script file on public repo. 
+# 
+# use cmd tasklist 
+
+
