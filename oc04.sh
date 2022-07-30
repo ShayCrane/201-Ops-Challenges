18ca658fc8edd44f98ddb085006c21f95977d150
shannon <shannon@ubuntudev>
2022-07-30 19:28:47 +0000

    standardizes file names for organizational purposes.

---
+++ oc04.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Script: Ops Challenge 04
+# Author: Shannon Crane
+# Date of latest revision: 07/28/2022
+# Purpose: a script that creates 4 directories using an array
+
+# define an array that creates 4 directories inside
+
+first_array=(mkdir{"dir1" "dir2" "dir3" "dir4"})
+
+
+${first_array[0]}
+
+
+# create 4 directories
+
+
+# mkdir ~/201_Ops_Challenges/d0
+# mkdir ~/201_Ops_Challenges/d1
+# mkdir ~/201_Ops_Challenges/d2
+# mkdir ~/201_Ops_Challenges/d3
+
+# define an array 
+
+# reference an array, put files into matching directories
+
+# $[first=array3]
+
+
+
+
+
+
