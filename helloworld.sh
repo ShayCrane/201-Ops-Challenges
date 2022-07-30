5d6d1b4dc11248b449bf4df2ef4859a86a94cc91
shannon <shannon@ubuntudev>
2022-07-30 21:15:25 +0000

    updated text formatting.

--- helloworld.sh
+++ helloworld.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Script:                       	Ops 201 Class 02 Ops Challenge 02
 # Author:                       	Shay Crane
-# Date of latest revision:     7/26/2022
+# Date of latest revision:          7/26/2022
 
 # Purpose:     			Print a string to the terminal
 # Main
