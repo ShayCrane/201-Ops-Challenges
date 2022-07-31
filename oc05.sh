a9eeb91db86de17460c120d181545df4373d7d61
shannon <shannon@ubuntudev>
2022-07-31 10:09:53 +0000

    another draft, I almost have it now.

--- oc05.sh
+++ oc05.sh
@@ -8,7 +8,7 @@
 # write a script that displays running processes, asks the user for a PID,
 # then kills the process with that PID. 
 # 
-# 	for this part of the lab, initialize a process that wont harm the OSs
+# 	initialize a process that wont harm the OSs
 #	 functionality upon termination.  dont kill esst'l processes required 
 #	for OS to work: kernel, drivers...
 
@@ -20,18 +20,37 @@
 # 
 # use cmd tasklist 
 
-ps -aux | less
+# Figured out how to make this script using a combination of
+# this oracle document online: https://docs.oracle.com/cd/E19253-01/817-0403/eoizf/index.html
+# and at this url: https://itsfoss.com/how-to-find-the-process-id-of-a-program-and-kill-it-quick-tip/
+
+
+ps -fu shannon
+
+VAR1=(YES NO)
+    echo Terminate process using PID? 
+    echo YES to continue, Enter NO to quit termination process
+    read ANSWER
+while [ANSWER = ${VAR1[1]}]
+do
+    echo enter PID to terminate
+    read VAR3
+    sudo kill -9 $VAR3
+    echo $VAR3 has been terminated. 
+done
+
+
+
+
+# ps -aux | less
 # above script found at https://www.cyberciti.biz/faq/show-all-running-processes-in-linux/
 
-echo "enter PID of process to terminate"
-while true
+# echo "enter PID of process to terminate"
+# while true
 
-do
+# do
 
-read -p 
+# read -p 
 
-echo $REPLY
-while $REPLY=
-
-
-
+# echo $REPLY
+# while $REPLY=
