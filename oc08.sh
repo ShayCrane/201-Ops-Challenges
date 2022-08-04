e96318a81bb7760e76d7a8cec7947e1e3184c0e2
shannon <shannon@ubuntudev>
2022-08-04 06:03:46 +0000

    adding challenge 08 to repo

---
+++ oc08.sh
@@ -0,0 +1,24 @@
+!#/bin/bash
+
+# Script: Ops Chllg 05
+# Author: Shay Crane
+# Date of latest revisions: 08/03/2022
+# PUrpose: to create an automated task that would save a backup copy of users documents at login.
+
+
+
+ROBOCOPY C:\Users\j.thomson\Desktop\myprojectfiles C:\Users\j.thompson\Desktop\Backup
+
+
+# Jorge's Backup:
+# I’ve been manually copying my work files on the desktop every night 
+# before I head home. I was curious if there’s a way to automate this process? 
+# Just curious, thanks again!”
+
+# Objectives
+# Automate the copy operation for Jorge
+
+
+# Resources
+# How to Use Windows Batch File Commands to Automate Repetitive Tasks
+# Wikibooks: Windows Batch Scripting
