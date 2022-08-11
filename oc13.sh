732d6619f15ff4eaa9fa77cf2ea03cd0263b2fc9
shannon <shannon@ubuntudev>
2022-08-11 05:55:24 +0000

    adds oc13.sh to repo

---
+++ oc13.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Script: Ops Callenge 13 - Domain Analyzer
+# Author: Shay Crane 
+# Date of latest revisions: 08/10/2022
+# PUrpose: a script that asks a user to type a domain, then displays document about
+# typed domain. To include commands: whois, dig, host, nslookup
+# 
+# https://www.howtogeek.com/680086/how-to-use-the-whois-command-on-linux/
+
+
+echo Enter Domain
+    read domain_name
+
+function domain_document {
+    nslookup $domain_name >> domain_information.txt
+    whois $domain_name >> domain_information.txt
+    dig $domain_name >> domain_information.txt
+}
+
+domain_document 
+nano domain_information.txt
+done
