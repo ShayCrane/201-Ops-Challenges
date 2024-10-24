e230e83144ea61ef1432370030f1a2fcecec1571
Shay Crane <109041611+ShayCrane@users.noreply.github.com>
2024-10-24 10:46:47 -0700

    Update oc02-helloworld.sh
    minor updates to visual formatting

--- oc02-helloworld.sh
+++ oc02-helloworld.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-# Script:                       	Ops 201 Class 02 Ops Challenge 02
-# Author:                       	Shay Crane
-# Date of latest revision:          7/26/2022
+# Script: Ops 201 Class 02 Ops Challenge 02
+# Purpose: Print a string to the terminal
+# Author: Shay Crane
+# Date of latest revision: 7/26/2022
 
-# Purpose:     			Print a string to the terminal
+# ----------------------
+
 # Main
 var="Hello World"
 
